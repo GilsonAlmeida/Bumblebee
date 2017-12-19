ab5a117587dedd99ae426e562034e17cc9ed7893
GILSON RICARDO DE ALMEIDA <gilson.almeida@elotech.com.br>
2017-12-19 11:16:38 -0200

    add projeto

---
+++ booblebee/src/main/java/br/com/booblebee/BooblebeeApplication.java
@@ -0,0 +1,12 @@
+package br.com.booblebee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooblebeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooblebeeApplication.class, args);
+	}
+}
